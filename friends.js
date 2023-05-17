6d90c7e19b0b796d04c750a9e9ed176c770bdb77
CedricLanglais <ced.langlais@yahoo.fr>
2023-05-17 21:44:02 +0200

    []

--- friends.js
+++ friends.js
@@ -1,20 +1,30 @@
 class Friend {
-    attributs(id, name, paragraphe) {
+    constructor(id, name, paragraphe) {
         this.id = id;
         this.name = name;
         this.paragraphe = paragraphe;
         this.likes = 0;
+        document.getElementById("friend" + id).onclick = event => {this.addLike()}
+        document.getElementById("friend" + id + id).onclick = event => {this.removeLike()}
+
     }
 
     addLike() {
         this.likes++;
-        like();
+        updateLikeDisplay();
+        console.log(this.name + " has this many likes : " + this.likes);
+
+        
     }
 
     removeLike() {
         if (this.likes > 0) {
             this.likes--;
-            like();
+            updateLikeDisplay();
+            console.log(this.name + " has this many likes : " + this.likes);
+        }
+        else {
+            console.log("on a 0 like");
         }
     }
 
@@ -24,19 +34,12 @@ let john = new Friend(1, 'John', 'John est un ami proche de feur');
 let emily = new Friend(2, 'Emily', 'Emily est une amie de feur wtf?');
 let mike = new Friend(3, 'Mike', 'Mike est un ami de andonigger.');
 
-let likeCount = 0
-
-function addLike(friendId) {
-    let friendElement = document.getElementById('friend' + friendId);
-    let likeButton = friendElement.querySelector('button');
-    likeCount++;
-    likeButton.innerText = likeCount + ' Likes';
-}
-function like() {
+function updateLikeDisplay() {
     const friends = [john, emily, mike];
     for (let i = 0; i < friends.length; i++) {
-        const friend = friends[i];
-        const likesElement = document.getElementById(`likes`);
+        let friend = friends[i];
+        let likesElement = document.getElementById("likes" + friend.id);
+        console.log("Getting the following id :" + i+1);
         likesElement.textContent = friend.likes;
     }
 
@@ -44,37 +47,23 @@ function like() {
     let maxLikesFriend = null;
 
     for (let i = 0; i < friends.length; i++) {
-        const friend = friends[i];
+        let friend = friends[i];
         if (friend.likes > maxLikes) {
             maxLikes = friend.likes;
             maxLikesFriend = friend;
         }
     }
+    for (let i = 0; i < friends.length; i++) {
+        let friend = friends[i];
+        let nameElement = document.getElementById("likes" + friend.id);
+        nameElement.style.fontWeight = (friend === maxLikesFriend ? 'bold' : 'normal');
+    }
 }
 
-// Section pour mettre en gras le nom de le plus liké //
 
-for (let i = 0; i < friend.length; i++) {
-    const friend = friend[i];
-    const nameElement = document.getElementById(`name`);
-    nameElement.style.fontWeight = friend === maxLikesFriend ? 'bold' : 'normal';
-}
+
+// Section pour mettre en gras le nom le plus liké //
+
+//
 
 // Section pour faire fonctionner les boutons removeLike //
-
-let John = document.getElementById("friend11");
-let Emily = document.getElementById("friend22");
-let Mike = document.getElementById("friend33");
-
-
-john.removeLike = function () {
-    console.log("Like removed for John");
-};
-
-emily.removeLike = function () {
-    console.log("Like removed for Emily");
-};
-
-mike.removeLike = function () {
-    console.log("Like removed for Mike");
-};
